cf0cb8f737f6055e804211e2204a2f3ee61f1e2f
Radit73 <borodo321@gmail.com>
2025-09-26 09:55:07 +0700

    Pert3

---
+++ 12524024_Afrizal/Pertemuan3/Praktikum3.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+
+void main() {
+  print("Masukkan Tanggal: ");
+  var inputTanggal = stdin.readLineSync();
+  print("Masukkan Bulan: ");
+  var inputBulan = stdin.readLineSync();
+  print("Masukkan Tahun: ");
+  var inputTahun = stdin.readLineSync();
+
+  var tanggal = int.tryParse(inputTanggal ?? "");
+  var bulan = int.tryParse(inputBulan ?? "");
+  var tahun = int.tryParse(inputTahun ?? "");
+
+  if (tanggal == null || bulan == null || tahun == null) {
+    print("Hanya menerima format Angka");
+  } else if (tanggal < 1 || tanggal > 31) {
+    print("Anda Salah Memasukan Tanggal");
+  } else if (bulan < 1 || bulan > 12) {
+    print("Anda Salah Memasukan Bulan");
+  } else if (tahun < 1000 || tahun > 2999) {
+    print("Anda Salah Memasukan Tahun");
+  } else {
+    print("Output = $tanggal - $bulan - $tahun");
+  }
+}
