82f4e13d5f19968b2781022284715d3525bc0803
widyapratamakaputri <widyapratamaep15@gmail.com>
2025-10-03 04:31:55 +0700

    tambahkan tugas lulu

---
+++ 12524030_LULU KHANSA/praktikum4.dart
@@ -0,0 +1,18 @@
+void loopingPertama() {
+  print("LOOPING PERTAMA");
+  for (int i = 2; i <= 20; i += 2) {
+    print("$i - I love coding ❤ 😊");
+  }
+}
+
+void loopingKedua() {
+  print("\nLOOPING KEDUA");
+  for (int i = 20; i >= 2; i -= 2) {
+    print("$i - I will become a mobile developer ❤ 😍");
+  }
+}
+
+void main() {
+  loopingPertama();
+  loopingKedua();
+}
