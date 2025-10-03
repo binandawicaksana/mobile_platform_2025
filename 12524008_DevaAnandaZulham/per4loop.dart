7b5d7ca488ec491ba9956cdab6f1cfc755ef4391
Deva <devaanandazulham07@gmail.com>
2025-10-03 09:56:18 +0700

    loop deva

---
+++ 12524008_DevaAnandaZulham/per4loop.dart
@@ -0,0 +1,25 @@
+void main() {
+  // Panggil fungsi looping pertama
+  loopingPertama();
+
+  print(""); // baris kosong
+
+  // Panggil fungsi looping kedua
+  loopingKedua();
+}
+
+// Fungsi untuk looping pertama
+void loopingPertama() {
+  print("LOOPING PERTAMA");
+  for (int i = 2; i <= 20; i += 2) {
+    print("$i - I love coding");
+  }
+}
+
+// Fungsi untuk looping kedua
+void loopingKedua() {
+  print("LOOPING KEDUA");
+  for (int i = 20; i >= 2; i -= 2) {
+    print("$i - I will become a mobile developer");
+  }
+}
