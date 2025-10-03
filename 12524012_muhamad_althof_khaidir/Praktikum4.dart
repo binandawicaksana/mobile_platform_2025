d281c58487c2288b3da159ca3431323c9c1ce1b9
Althofkhaidir1802 <althofkhaidir1802@gmail.com>
2025-10-03 09:57:29 +0700

    althof

---
+++ 12524012_muhamad_althof_khaidir/Praktikum4.dart
@@ -0,0 +1,19 @@
+void main() {
+  loopingPertama();
+
+  loopingKedua();
+}
+
+void loopingPertama() {
+  print("LOOPING PERTAMA");
+  for (int i = 2; i <= 20; i += 2) {
+    print("$i - I love coding");
+  }
+}
+
+void loopingKedua() {
+  print("LOOPING KEDUA");
+  for (int i = 20; i >= 2; i -= 2) {
+    print("$i - I will become a mobile developer");
+  }
+}
