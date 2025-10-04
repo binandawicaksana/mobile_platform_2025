29b2380412363a2c5f43fa0aff4db46ec5d4c249
Daud Zulfikar <daudzulfikar7@gmail.com>
2025-10-04 22:05:38 +0700

    Menambahkan praktikum4.dart

---
+++ 12524020_DaudZulfikar/praktikum4.dart
@@ -0,0 +1,25 @@
+void main() {
+  loopingPertama();
+  print(""); // baris kosong
+  loopingKedua();
+}
+
+// Fungsi untuk looping pertama
+void loopingPertama() {
+  print("LOOPING PERTAMA");
+  for (int i = 2; i <= 20; i += 2) {
+    print("$i - I love MySelf");
+  }
+}
+
+// Fungsi untuk looping kedua
+void loopingKedua() {
+  print("LOOPING KEDUA");
+  for (int i = 20; i >= 2; i -= 2) {
+    print("$i - Daud Zulfikar will become a mobile developer");
+  }
+}
+
+//Daud Zulfikar
+//12524020
+//Teknik Informatika Semester 3
